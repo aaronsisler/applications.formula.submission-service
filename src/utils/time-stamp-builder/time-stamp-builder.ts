5b8c2f8253e5b22dcaa0f7c574c78e6b83cab396
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2021-07-31 20:33:15 -0400

    Finalizing the time stamp for EST

--- src/utils/time-stamp-builder/time-stamp-builder.ts
+++ src/utils/time-stamp-builder/time-stamp-builder.ts
@@ -1,6 +1,17 @@
 const timeStampBuilder = () => {
-  const now = new Date(Date.now());
-  return `${now.getFullYear()}-${now.getMonth()}-${now.getDay()} ${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}:${now.getMilliseconds()}`;
+  const EST_OFFSET_HOURS: number = 4;
+  const timeOffsetInMS: number = EST_OFFSET_HOURS * 3600000;
+  const nowUtc = new Date(Date.now());
+  const now = new Date(nowUtc.getTime() - timeOffsetInMS);
+
+  const month = `0${now.getMonth() + 1}`.slice(-2);
+  const date = `0${now.getDate()}`.slice(-2);
+  const hours = `0${now.getHours()}`.slice(-2);
+  const minutes = `0${now.getMinutes()}`.slice(-2);
+  const seconds = `0${now.getSeconds()}`.slice(-2);
+  const timestamp = `${now.getFullYear()}-${month}-${date} ${hours}:${minutes}:${seconds}`;
+
+  return timestamp;
 };
 
 export { timeStampBuilder };
